9ed5eaa034c3c59003a1b0894acca04aa9a048cf
rheajoywalters <54914771+rheajoywalters@users.noreply.github.com>
2019-10-09 13:37:15 -0600

    Initial Commit with Firebase Settings

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyDnT749pto7K9C5N6VvS0aTVvFMJq6QSzI",
-  authDomain: "authentication-d0e33.firebaseapp.com",
-  databaseURL: "https://authentication-d0e33.firebaseio.com",
-  projectId: "authentication-d0e33",
-  storageBucket: "",
-  messagingSenderId: "310675757578",
-  appId: "1:310675757578:web:9e17f766afbce2075bddf3"
+  apiKey: "AIzaSyB4QQAPtn56DyIZO9b3iH9isSgAk4Jl3jU",
+  authDomain: "todo-f852a.firebaseapp.com",
+  databaseURL: "https://todo-f852a.firebaseio.com",
+  projectId: "todo-f852a",
+  storageBucket: "todo-f852a.appspot.com",
+  messagingSenderId: "976982143493",
+  appId: "1:976982143493:web:a16ee04f76597e7a80a26c",
+  measurementId: "G-Q3KTNQ1P1N"
 };
 
 firebase.initializeApp(config);
