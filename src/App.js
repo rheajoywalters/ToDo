4fe6e7c9ac5ce71c90d1ff1efa269774819618bb
rheajoywalters <54914771+rheajoywalters@users.noreply.github.com>
2019-10-20 09:44:49 -0600

    Final ToDo App

--- src/App.js
+++ src/App.js
@@ -6,162 +6,19 @@ import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import Drawer from "@material-ui/core/Drawer";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Link } from "react-router-dom";
-import { auth } from "./firebase";
-
-export function SignIn(props) {
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => {
-      if (u) {
-        props.history.push("/app");
-      }
-      // do something
-    });
-
-    return unsubscribe;
-  }, [props.history]);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignIn = () => {
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {})
-      .catch(error => {
-        alert(error.message);
-      });
-  };
-
-  return (
-    <div>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Sign In
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Paper style={{ width: "480px", marginTop: "50px", padding: "30px" }}>
-          <TextField
-            placeholder={"Email"}
-            fullWidth={true}
-            value={email}
-            onChange={e => {
-              setEmail(e.target.value);
-            }}
-          />
-          <TextField
-            type={"password"}
-            placeholder={"Password"}
-            fullWidth={true}
-            style={{ marginTop: "30px" }}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value);
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginTop: "30px"
-            }}
-          >
-            <Typography>
-              Don't have an account? <Link to="/signup">Sign up!</Link>
-            </Typography>
-            <Button color="primary" variant="contained" onClick={handleSignIn}>
-              Sign in
-            </Button>
-          </div>
-        </Paper>
-      </div>
-    </div>
-  );
-}
-
-export function SignUp(props) {
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => {
-      if (u) {
-        props.history.push("/app");
-      }
-      // do something
-    });
-
-    return unsubscribe;
-  }, [props.history]);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignUp = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {})
-      .catch(error => {
-        alert(error.message);
-      });
-  };
-
-  return (
-    <div>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Sign Up
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Paper style={{ width: "480px", marginTop: "50px", padding: "30px" }}>
-          <TextField
-            placeholder={"Email"}
-            fullWidth={true}
-            value={email}
-            onChange={e => {
-              setEmail(e.target.value);
-            }}
-          />
-          <TextField
-            type={"password"}
-            placeholder="Password"
-            fullWidth={true}
-            style={{ marginTop: "30px" }}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value);
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginTop: "30px"
-            }}
-          >
-            <Typography>
-              Already have an account? <Link to="/">Sign in!</Link>
-            </Typography>
-            <Button color="primary" variant="contained" onClick={handleSignUp}>
-              Sign Up
-            </Button>
-          </div>
-        </Paper>
-      </div>
-    </div>
-  );
-}
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { auth, db } from "./firebase";
 
 export function App(props) {
-  const [drawer_open, setDrawerOpen] = useState(false);
   const [user, setUser] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(u => {
@@ -175,9 +32,28 @@ export function App(props) {
     return unsubscribe;
   }, [props.history]);
 
-  if (!user) {
-    return <div />;
-  }
+  useEffect(() => {
+    let unsubscribe;
+
+    if (user) {
+      unsubscribe = db
+        .collection("users")
+        .doc(user.uid)
+        .collection("tasks")
+        .onSnapshot(snapshot => {
+          const user_tasks = snapshot.docs.map(qs => {
+            const user_task = {
+              id: qs.id,
+              text: qs.data().text,
+              complete: qs.data().complete
+            };
+            return user_task;
+          });
+          setTasks(user_tasks);
+        });
+    }
+    return unsubscribe;
+  }, [user]);
 
   const handleSignOut = () => {
     auth
@@ -186,45 +62,115 @@ export function App(props) {
         props.history.push("/");
       })
       .catch(error => {
-        alert(error.message);
+        console.log(error.message);
       });
+  };
+
+  if (!user) return <div />;
+
+  const addTask = () => {
+    db.collection("users")
+      .doc(user.uid)
+      .collection("tasks")
+      .add({ text: newTask, checked: false })
+      .then(() => {
+        setNewTask("");
+      });
+  };
+
+  const deleteTask = task_id => {
+    db.collection("users")
+      .doc(user.uid)
+      .collection("tasks")
+      .doc(task_id)
+      .delete();
+  };
+
+  const changeCheck = (checked, task_id) => {
+    db.collection("users")
+      .doc(user.uid)
+      .collection("tasks")
+      .doc(task_id)
+      .update({ checked: checked });
   };
 
   return (
     <div>
-      <AppBar position="static" color="primary">
+      <AppBar color="secondary" position="static">
         <Toolbar>
-          <IconButton
-            color="inherit"
-            onClick={() => {
-              setDrawerOpen(true);
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
-            variant="h6"
             color="inherit"
-            style={{ flexGrow: 1, marginLeft: "30px" }}
+            variant="h6"
+            style={{ marginLeft: 15, flexGrow: 1 }}
           >
-            My App
+            To Do List
           </Typography>
-          <Typography color="inherit" style={{ marginRight: "30px" }}>
-            Hi {user.email}!
+          <Typography color="inherit" style={{ marginRight: 30 }}>
+            DON'T BE STRESSED {user.email}!:)
           </Typography>
-          <Button color="inherit" onClick={handleSignOut}>
-            Sign out
+          <Button onClick={handleSignOut} color="inherit">
+            Sign Out
           </Button>
         </Toolbar>
       </AppBar>
-      <Drawer
-        open={drawer_open}
-        onClose={() => {
-          setDrawerOpen(false);
-        }}
-      >
-        <div>Oh Heeeeey it's drawer</div>
-      </Drawer>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 30 }}>
+        <Paper
+          style={{
+            maxWidth: "500px",
+            width: "100%",
+            padding: "30px",
+            marginTop: "40px"
+          }}
+        >
+          <Typography color="secondary" variant="h6">
+            To Do List
+          </Typography>
+          <div style={{ display: "flex", marginTop: 30 }}>
+            <TextField
+              fullWidth={true}
+              placeholder="Be Calm and Task On:"
+              style={{ marginRight: "30px" }}
+              value={newTask}
+              onChange={e => {
+                setNewTask(e.target.value);
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginLeft: "30px" }}
+              onClick={addTask}
+            >
+              Add
+            </Button>
+          </div>
+          <List>
+            {tasks.map(value => (
+              <ListItem key={value.id}>
+                <ListItemIcon>
+                  <Checkbox
+                    checked={value.checked}
+                    onChange={(e, checked) => {
+                      changeCheck(checked, value.id);
+                    }}
+                    // checked = seomthing
+                  />
+                </ListItemIcon>
+                <ListItemText primary={value.text} />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    onClick={() => {
+                      deleteTask(value.id);
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </div>
     </div>
   );
 }
